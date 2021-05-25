a9de875598889a33041f46c88e461b21c56acf0c
haztecaso <adrianlattes@disroot.org>
2021-05-25 09:53:22 +0200

    reescribiendo funciones para crear archivos de resumenes

--- bicimad/resumir.py
+++ bicimad/resumir.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from bicimad import JsonFiles
+from bicimad import DataLoader
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
 
 PATH = 'file:///home/alattes/'
 
@@ -10,26 +11,20 @@ def sample_n(df:DataFrame, n:int):
     fraction = n / df.count()
     return df.sample(False, fraction)
 
-def sample(**kwargs):
-    n_movements, n_stations = kwargs.get('n_movements', 100), kwargs.get('n_stations', 10)
-    years, months = kwargs.get('years', None), kwargs.get('months', None)
-    sample_name = lambda f,n: f"{f.split('/')[-1].split('.')[0]}_rand_{n}.sample"
-    files = JsonFiles(appName="bicimad-resumen")
+def sample_01(data_loader:DataLoader):
+    df_last_month = data_loader.get_df(start="202103", end="202103", type="movements")
+    sample_n(df_last_month, 1000).coalesce(1).write\
+            .save(path = '202103_movements_random_sample_1000',
+                    format = 'json', mode = 'overwrite')
 
-    data = files.get(years=years, months=months, path=True)
-    print("These files will be created:")
-    for f in data:
-        n = n_movements if 'movements' in f else n_stations
-        print('·', sample_name(f, n))
-    input('Press enter to confirm (ctrl-c to cancel).')
-    
-    data = files.get_dfs(filenames=True, years=years, months=months)
-    for f, df in data:
-        n = n_movements if 'movements' in f else n_stations
-        print(f'Saving sample: {sample_name(f,n)}')
-        sample_n(df, n).coalesce(1).write\
-                .save(path=sample_name(f,n), format='json', mode='overwrite')
-
+def sample_02(data_loader:DataLoader):
+    df = data_loader.get_df(type="movements")\
+            .filter(col('track').isNotNull())
+    sample_n(df, 100).coalesce(1).write\
+            .save(path = 'movements_gps_random_sample_100',
+                    format = 'json', mode = 'overwrite')
 
 if __name__ == '__main__':
-    sample()
+    data = DataLoader(appName = "bicimad-resumen")
+    # sample_01(data)
+    # sample_02(data)
