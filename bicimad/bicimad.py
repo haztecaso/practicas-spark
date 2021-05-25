28ff37f98c0bf2887b030082020730885f4482b5
haztecaso <adrianlattes@disroot.org>
2021-05-25 09:52:58 +0200

    renombrando JsonFiles a DataLoader y cambiando comportamiento del metodo get

--- bicimad/bicimad.py
+++ bicimad/bicimad.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-import subprocess, pickle, re
+import pickle
+import re
+import subprocess
 from datetime import datetime
 from operator import itemgetter
-from pyspark.sql import SparkSession, DataFrame
-from functools import reduce
+
+from pyspark.sql import DataFrame, SparkSession
 
 LS_REGEX = re.compile(r'^([-|r|w]+)\s+\d+\s+[^\d]*(\d+)[^/]+(/.+)$', re.MULTILINE)
 LS_FILEINFO = re.compile(r'^[^\d]+(\d+)_(\w*)\.json$')
@@ -12,7 +14,7 @@ DATE_FORMAT = '%Y%m'
 is_iterable = lambda obj: hasattr(obj, '__iter__')
 
 
-class JsonFiles():
+class DataLoader():
     def __init__(self, **kwargs):
         self.dir = kwargs.get('dir','/public_data/bicimad')
         self.cache = kwargs.get('cache_file', 'cache_files.data')
@@ -22,36 +24,32 @@ class JsonFiles():
         else:
             self.spark = None
         self.files = []
-        self.load_cache()
+        self._load_cache()
         if len(self.files) == 0:
-            self.load_hdfs()
-            self.write_cache()
+            self._load_hdfs()
+            self._write_cache()
 
     def get(self, **kwargs):
-        result = self.files
-        f_check = lambda f_key, arg_key: (lambda f: f[f_key] in kwargs[arg_key])
-        if kwargs.get('years', None):
-            assert is_iterable(kwargs['years'])
-            result = filter(f_check('year','years'), result)
-        if kwargs.get('months', None): 
-            assert is_iterable(kwargs['months'])
-            result = filter(f_check('month', 'months'), result)
+        date_from = kwargs.get('start', self.files[0]['date'])
+        if type(date_from) == str:
+            date_from = datetime.strptime(date_from, DATE_FORMAT)
+        date_to = kwargs.get('end', self.files[-1]['date'])
+        if type(date_to) == str:
+            date_to = datetime.strptime(date_to, DATE_FORMAT)
+        files = filter(lambda f: f['date'] >= date_from and f['date'] <= date_to, self.files)
         if 'type' in kwargs:
-            result = filter(lambda f: f['type'] == kwargs['type'], result)
+            assert kwargs['type'] in ['movements', 'stations'], 'Invalid type'
+            files = filter(lambda f: f['type'] == kwargs['type'], files)
         if kwargs.get('path', False):
-            result = map(itemgetter('path'), result) 
-        return result
+            files = map(itemgetter('path'), files)
+        return files
 
-    def get_dfs(self, **kwargs):
-        selection = self.get(**kwargs, path=True)
+    def get_df(self, **kwargs):
+        files = list(self.get(**kwargs, path=True))
+        print(f'Reading {len(files)} json files into DataFrame')
         self._init_spark()
-        for f in selection:
-            print(f'Reading {f} into DataFrame')
-            df = self.spark.read.json(f)
-            if kwargs.get('filenames', False):
-                yield (f,df)
-            else:
-                yield df
+        df = self.spark.read.json(files)
+        return df
 
     def _init_spark(self):
         if not self.spark:
@@ -59,7 +57,7 @@ class JsonFiles():
                     .appName(self.appName)\
                     .getOrCreate()
 
-    def load_hdfs(self):
+    def _load_hdfs(self):
         print(f'Getting list of json files in hdfs folder {self.dir}')
         cmd = f'/opt/hadoop/current/bin/hdfs dfs -ls {self.dir}'.split(' ')
         cmd_out = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode()
@@ -79,22 +77,23 @@ class JsonFiles():
                 })
         self.files.sort(key=itemgetter('date'))
 
-    def write_cache(self):
+    def _write_cache(self):
         assert len(self.files) > 0, 'Cannot cache empty list of files' 
         with open(self.cache, 'wb') as cache:
             pickle.dump(self.files, cache)
 
-    def load_cache(self):
+    def _load_cache(self):
         try:
             with open(self.cache, 'rb') as cache:
-                print(f'Loading cache file {self.cache}')
+                print(f"'Loading cache file '{self.cache}'")
                 self.files = pickle.load(cache)
         except FileNotFoundError:
-            pass
+            print("Cache file '{self.cache}' not found")
 
 
 def main():
-    files = JsonFiles().get(type="movements", path=False)
+    data = DataLoader()
+    files = data.get(type="movements", path=False)
     days = 0
     from calendar import monthrange
     for f in files:
